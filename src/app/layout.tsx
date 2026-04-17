6e769692440040e2f37411d85e6c297b6e31117a
cortins-05 <lucasortins@gmail.com>
2026-04-17 12:53:47 +0200

    Ajustar formato del cuerpo en el componente RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${montserratAlternates.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
