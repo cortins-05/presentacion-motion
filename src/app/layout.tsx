fd08290c9815c4667ca8bab9664de175cfd09c25
cortins-05 <lucasortins@gmail.com>
2026-04-17 12:53:31 +0200

    Agregar soporte para la fuente Montserrat Alternates en el diseño

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Montserrat_Alternates } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +11,12 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const montserratAlternates = Montserrat_Alternates({
+  variable: "--font-mont-alternates",
+  weight: ["400","700","900"],
+  subsets: ["latin"]
+})
 
 export const metadata: Metadata = {
   title: "Presentacion MOTION",
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${montserratAlternates.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
